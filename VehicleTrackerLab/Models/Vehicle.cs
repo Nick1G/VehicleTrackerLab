b549d1c21c6634a67eb4125bbdfb8c59edd6db08
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2022-05-07 21:25:20 -0500

    Add project files.

---
+++ VehicleTrackerLab/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleTrackerLab.Models
+{
+    public class Vehicle
+    {
+        public string Licence { get; set; }
+        public bool Pass { get; set; }
+
+        public Vehicle(string licence, bool pass)
+        {
+            this.Licence = licence;
+            this.Pass = pass;
+        }
+    }
+}
