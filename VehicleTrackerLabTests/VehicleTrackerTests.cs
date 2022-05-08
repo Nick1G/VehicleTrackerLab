124f41ae2cc9676cc7a9895b92917f11c77ecd1c
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2022-05-07 22:24:59 -0500

    Adding test for AddVehicle method if there are no slots available.

--- VehicleTrackerLabTests/VehicleTrackerTests.cs
+++ VehicleTrackerLabTests/VehicleTrackerTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using VehicleTrackerLab.Models;
 
 namespace VehicleTrackerLabTests
@@ -22,13 +23,29 @@ namespace VehicleTrackerLabTests
         public void AddVehicleMethod_IfSlotsAvailable_AddsNewVehicle()
         {
             VehicleTracker vt = new VehicleTracker(1, "Bingo drive");
-            Vehicle vehicle = new Vehicle("R2E 7AY", false);
+            Vehicle vh = new Vehicle("R2E 7AY", false);
 
-            vt.AddVehicle(vehicle);
-            var expectedVehicle = vehicle;
+            vt.AddVehicle(vh);
+            var expectedVehicle = vh;
             var actualVehicle = vt.VehicleList[1];
 
             Assert.AreSame(expectedVehicle, actualVehicle);
         }
+
+        [TestMethod]
+        public void AddVehicleMethod_IfSlotsUnavailable_ThrowsException()
+        {
+            VehicleTracker vt = new VehicleTracker(0, "Boingus blvd");
+            Vehicle vh = new Vehicle("T7Y YY9", true);
+
+            Assert.ThrowsException<IndexOutOfRangeException> (() => vt.AddVehicle(vh));
+        }
+
+        //public void RemoveVehicleMethod_IfValidData_RemovesVehicle()
+        //{
+        //    VehicleTracker vt = new VehicleTracker(3, "Spoink road");
+        //    Vehicle vh = new Vehicle("KXN 787", false);
+        //    Vehicle vh2 = new Vehicle("")
+        //}
     }
 }
